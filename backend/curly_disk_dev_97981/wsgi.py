bfafd251add310cf1be16da66a6b94a7a56fe87d
Crowdbotics <engineering@crowdbotics.com>
2023-07-05 23:15:47 +0000

    Initial commit.

---
+++ backend/curly_disk_dev_97981/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for curly_disk_dev_97981 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'curly_disk_dev_97981.settings')
+
+application = get_wsgi_application()
